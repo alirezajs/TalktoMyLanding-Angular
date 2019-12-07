38f62e337a47d048d6208c1b5e752c49ea9f3adc
alirezajs <alireza.varmaghani@gmail.com>
2019-12-07 23:58:10 +0330

    Add new components and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FeatureComponent } from './components/feature/feature.component';
+import { ServiceComponent } from './components/service/service.component';
+import { ScreenShotComponent } from './components/screen-shot/screen-shot.component';
+import { PostComponent } from './components/post/post.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FeatureComponent,
+    ServiceComponent,
+    ScreenShotComponent,
+    PostComponent,
+    ContactUsComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
