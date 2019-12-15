da15a8fdea4c56bfacf9fb0ece2912447a40a812
alirezajs <alireza.varmaghani@gmail.com>
2019-12-15 22:23:57 +0330

    add time span

--- src/app/components/comment-card/comment-card.component.ts
+++ src/app/components/comment-card/comment-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IComment } from '../../models/comment.interface'
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-comment-card',
   templateUrl: './comment-card.component.html',
@@ -15,7 +16,26 @@ export class CommentCardComponent implements OnInit {
   ngOnInit() {
     console.log(this.comment);
   }
-  public getTimeSpan() {
 
+  public getTimeSpan(value: number) {
+    if (value) {
+      const date = new Date(value).getTime();
+      let now = new Date().getTime();
+      let delta = ((now - date) / 1000);
+
+      if (delta < 10) {
+        return 'همین الان';
+      }
+      if (delta < 60) { // sent in last minute
+        return Math.floor(delta) + '  ثانیه قبل';
+      }
+      if (delta < 3600) { // sent in last hour
+        return Math.floor(delta / 60) + '  دقیقه قبل';
+      }
+      if (delta < 86400) { // sent on last day
+        return Math.floor(delta / 3600) + '  ساعت قبل';
+      }
+      return Math.floor(delta / 86400) + ' روز قبل';
+    }
   }
 }
